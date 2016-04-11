e34c6b99337c96f6715162a158164c35b3f8e436
Richard Castro Mamani <rcastro@hinantin.com>
2016-04-11 08:58:07 -0500

    Adding information on how to compile Malt server.

---
+++ MT_systems/maltparser_tools/src/compile.sh
@@ -0,0 +1,4 @@
+
+javac -cp /home/richard/Downloads/01_Instaladores/maltparser-1.8.1/maltparser-1.8.1.jar MaltParserServer.java 
+
+java MaltParserServer 1234 /home/richard/Documents/squoia/MT_systems/models/splitDatesModel.mco
