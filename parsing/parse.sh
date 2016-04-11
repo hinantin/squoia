8a5d1b1ec602ed83f047a43edd0b1611a630cb63
Richard Castro Mamani <rcastro@hinantin.com>
2016-04-11 08:41:21 -0500

    Adding modification to the parser.

--- parsing/parse.sh
+++ parsing/parse.sh
@@ -5,10 +5,11 @@
 ########################################################
 
 # path to the compiled xfst analyzers, either from the git repository, or the package squoiaMorph from https://github.com/ariosquoia/squoia/releases
-export NORMALIZER_DIR=/mnt/storage/hex/projects/clsquoia/arios_squoia_git/morphology/normalizer
+export NORMALIZER_DIR=/home/richard/Documents/Runa_Simi/06_Morphology/normalizer
 
 # Path to your maltparser installation (get MaltParser from http://www.maltparser.org/download.html)
-export MALTPARSER_DIR=/mnt/storage/hex/projects/clsquoia/parser/maltparser-1.8
+export MALTPARSER_DIR=/home/richard/Downloads/01_Instaladores/maltparser-1.8.1
+export WAPITI=/home/richard/Downloads/01_Instaladores/wapiti/wapiti-1.5.0/wapiti
 export TMP_DIR=tmp
 
 ## Models to disambiguate words
@@ -40,31 +41,29 @@ cat $RAW_FILE | perl splitSentences.pl | perl $NORMALIZER_DIR/tokenize.pl | look
 cat $TMP_DIR/test.xfst | perl cleanGuessedRoots.pl -$EVID -$PISPAS > $TMP_DIR/test_clean.xfst
 cat $TMP_DIR/test_clean.xfst | perl xfst2wapiti_pos.pl -test > $TMP_DIR/pos.test
 
-wapiti label -m $MORPH1_MODEL $TMP_DIR/pos.test > $TMP_DIR/morph1.result
+$WAPITI label -m $MORPH1_MODEL $TMP_DIR/pos.test > $TMP_DIR/morph1.result
 
 perl disambiguateRoots.pl $TMP_DIR/morph1.result $TMP_DIR/test_clean.xfst > $TMP_DIR/morph1.disamb
 
 perl xfst2wapiti_morphTest.pl -1 $TMP_DIR/morph1.disamb > $TMP_DIR/morph2.test
 
-wapiti label -m $MORPH2_MODEL $TMP_DIR/morph2.test > $TMP_DIR/morph2.result
+$WAPITI label -m $MORPH2_MODEL $TMP_DIR/morph2.test > $TMP_DIR/morph2.result
 
 perl xfst2wapiti_morphTest.pl -2 $TMP_DIR/morph2.result > $TMP_DIR/morph3.test
 
-wapiti label -m $MORPH3_MODEL $TMP_DIR/morph3.test > $TMP_DIR/morph3.result
+$WAPITI label -m $MORPH3_MODEL $TMP_DIR/morph3.test > $TMP_DIR/morph3.result
 
 perl xfst2wapiti_morphTest.pl -3 $TMP_DIR/morph3.result > $TMP_DIR/morph4.test
 
-wapiti label -m $MORPH4_MODEL $TMP_DIR/morph4.test > $TMP_DIR/morph4.result
+$WAPITI label -m $MORPH4_MODEL $TMP_DIR/morph4.test > $TMP_DIR/morph4.result
 
 perl xfst2wapiti_morphTest.pl -4 $TMP_DIR/morph4.result > $TMP_DIR/disamb.xfst
-
 
 # convert xfst to conll
 cat $TMP_DIR/disamb.xfst | perl xfst2conll.pl > $TMP_DIR/$filename_no_ext.conll
 
-
 # parse conll
-java -jar $MALTPARSER_DIR/maltparser-1.8.jar -c $MALTPARSER_MODEL -i $TMP_DIR/$filename_no_ext.conll -o $TMP_DIR/$filename_no_ext.parsed.conll -m parse
+java -jar $MALTPARSER_DIR/maltparser-1.8.1.jar -c $MALTPARSER_MODEL -i $TMP_DIR/$filename_no_ext.conll -o $TMP_DIR/$filename_no_ext.parsed.conll -m parse
 
 # convert conll to pml to view (and edit) trees in TrEd (see https://ufal.mff.cuni.cz/tred/)
 perl quzconll2pml.pl -s quz_schema.xml -n $filename_no_ext -c $TMP_DIR/$filename_no_ext.parsed.conll -t quz_stylesheet > $TMP_DIR/$filename_no_ext.pml 
