dcf63b1bdb0cd8a700c252390bdde718885ed0cb
Richard Castro Mamani <rcastro@hinantin.com>
2016-04-11 07:01:30 -0500

    Adding instructions on how to compile outputSentences.cpp

--- MT_systems/squoia/esqu/compile.sh
+++ MT_systems/squoia/esqu/compile.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -x #echo on
 
-# g++ -o outputSentences outputSentences.cpp -I/home/richard/Documents/kenlm -I/usr/include -I/home/richard/Downloads/01_Instaladores/freeling/FreeLing-4.0-beta1/src/libfoma/foma -DKENLM_MAX_ORDER=6 -L/home/richard/Documents/kenlm/build/lib -lkenlm
 STD=gnu++0x # This standard may vary acconding to your operating system
 CXX=${CXX:-g++}
-CXXFLAGS+=" -I. -O3 -DNDEBUG -DKENLM_MAX_ORDER=6"
-
-echo "$CXXFLAGS"
 
 rm -f outputSentences outputSentences.o compile.log
-# g++ -std=$STD -c -o outputSentences.o outputSentences.cpp -I/home/richard/Documents/kenlm -DKENLM_MAX_ORDER=6 -L/home/richard/Documents/kenlm/build/lib -lkenlm
-$CXX -std=gnu++0x -o outputSentences outputSentences.cpp -I/home/richard/Documents/kenlm -I/usr/include -DKENLM_MAX_ORDER=6 -L/home/richard/Documents/kenlm/build/lib -L/home/richard/Documents/kenlm/build/lib/ -lkenlm -lkenlm_util -lkenlm_filter -lkenlm_builder -L/usr/local/lib/ -lfoma -lz -L/usr/lib -lboost_regex -L/home/richard/Documents/bzip2-1.0.6/ -lbz2 2>&1 | tee -a compile.log
+
+# --- Compiling 
+# --- Linking the shared libraries (For linking <path_to_shared_object>/lib{name}.so write -L<path_to_shared_object>/ -l{name}) and static libraries (For linking <path_to_static_lib>/lib{name}.a write -L<path_to_static_lib>/ -l{name})
+$CXX -std=$STD -o outputSentences outputSentences.cpp -I/home/richard/Documents/kenlm -I/usr/include -DKENLM_MAX_ORDER=6 -L/home/richard/Documents/kenlm/build/lib -L/home/richard/Documents/kenlm/build/lib/ -lkenlm -lkenlm_util -lkenlm_filter -lkenlm_builder -L/usr/local/lib/ -lfoma -lz -L/usr/lib -lboost_regex -L/home/richard/Documents/bzip2-1.0.6/ -lbz2 2>&1 | tee -a compile.log
 
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-# g++ -std=$STD -O3 -Wall -o outputSentences outputSentences.o -L/usr/local/lib -L/home/richard/Documents/kenlm/build/lib -lkenlm -lboost_regex /home/richard/Downloads/01_Instaladores/freeling/FreeLing-4.0-beta1/src/libfoma/.libs/libfoma.a -lz
+# --- Testing
+# ./outputSentences -l -m /home/richard/Documents/squoia/MT_systems/models/all_morph_5grams_interpolated_unigr.lm -n 3 -i /home/richard/Documents/squoia/MT_systems/tmp/tmp.morph -f /home/richard/Documents/squoia/MT_systems/squoia/esqu/morphgen_foma/unificadoTransfer.fst
 
-# $CXX -o outputSentences outputSentences.cpp -I/home/richard/Documents/kenlm -I/home/richard/Documents/boost_1_60_0 -DKENLM_MAX_ORDER=6 -L/home/richard/Documents/kenlm/build/lib -L/home/richard/Documents/boost_1_60_0/stage/lib -lkenlm -lboost_regex /usr/local/lib/libfoma.a -lz 2>&1 | tee -a compile.log
