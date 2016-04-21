1436c565a5df4407c0a95ad90604dd081ca5f797
Richard Castro Mamani <rcastro@hinantin.com>
2016-04-21 12:41:09 -0500

    Adding instrauctions on how to compile the dictionary.

---
+++ MT_systems/squoia/esqu/lexica/compiledic.sh
@@ -0,0 +1,4 @@
+mv es-quz.bin es-quz.bin.$(date +%Y%m%d%H%M%S) 2>/dev/null
+
+/home/richard/Desktop/apertium/lttoolboxinstall/bin/lt-comp lr /home/richard/Documents/squoia/MT_systems/squoia/esqu/lexica/es-quz.dix /home/richard/Documents/squoia/MT_systems/squoia/esqu/lexica/es-quz.bin
+
